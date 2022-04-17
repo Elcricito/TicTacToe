c71fc5026d3189f684eb21904badbcdbe82345ed
Cristian Muñoz <88356235+Elcricito@users.noreply.github.com>
2022-04-16 23:13:55 -0500

    Primera versión del juego

---
+++ src/tictactoe/TicTacToe.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tictactoe;
+
+/**
+ *
+ * @author Cristian
+ */
+public class TicTacToe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Nombres ppal = new Nombres();
+        ppal.setVisible(true);
+    }
+    
+}
